ef64c7aaa1937fd10a73f94f9feefb372109b9ea
huifeng <huifeng@pinduoduo.com>
2023-02-20 17:05:00 +0800

    delivevpc

--- virtualnetwork/version.tf
+++ virtualnetwork/version.tf
@@ -6,5 +6,16 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 3.73"
     }
+
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.11"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "terraformadmin"
+    storage_account_name = "lterraformstorage"
+    container_name       = "terraform-state"
   }
 }
